be942bcfe9389bd547d9c27095eac24c341722d6
barry <940529189@qq.com>
2017-11-09 16:46:19 +0800

    增加登录注册组件

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
-    <router-view/>
-    <content-page></content-page>
+    <!-- <router-view/>
+    <content-page></content-page> -->
+    <regist></regist>
   </div>
 </template>
 
 <script>
-import ContentPage from '@/module/ContentPage'
+// import ContentPage from '@/module/ContentPage'
+// import Login from '@/module/Login/'
+import Regist from '@/module/Regist/'
 export default {
   name: 'app',
   components:{
-    ContentPage
+    // ContentPage,
+    // Login,
+    Regist
   }
 }
 </script>
