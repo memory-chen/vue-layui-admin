015139c3f967ad1c61daddf543665c0fd8f86456
beggarMen <1059326368@qq.com>
2017-11-13 10:05:46 +0800

    vuex插件添加

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
 require('layui-src/dist/css/layui.css')
 // require('jquery/dist/jquery.min.js')
 require('layui-src/dist/layui.js')
 require('../static/main.css')
+// 引入vuex插件。
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
