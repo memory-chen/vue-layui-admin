ce7d9439edd2954525aa2d7398f578e80645062d
barryPanlz <940529189@qq.com>
2017-11-15 15:21:36 +0800

    增加新信息组件功能

--- src/components/NewsTable.vue
+++ src/components/NewsTable.vue
@@ -8,60 +8,25 @@
         <col>
       </colgroup>
       <tbody class="msgHtml">
-        <tr>
-          <td class="msg_info"> <img src="../assets/logo.png" width="50" height="50"><input type="hidden" value="1111">
+        <tr v-for="(news,index) in tablist" :key="news.id" >
+          <td class="msg_info"> <img :src="news.img" width="50" height="50"><input type="hidden" value="1111">
             <div class="user_info">
-              <h2>小王</h2>
-              <p>马哥你好，请问你这个模版用的是什么框架啊？</p>
+              <h2>{{news.name}}</h2>
+              <p> {{news.content}} </p>
             </div>
           </td>
-          <td class="msg_time">2017-05-27 12:50</td>
-          <td class="msg_reply">已回复</td>
-          <td class="msg_opr"> <a class="layui-btn layui-btn-mini layui-btn-normal msg_collect"><i class="layui-icon"></i> 收藏</a> <a class="layui-btn layui-btn-mini reply_msg"><i class="layui-icon"></i> 回复</a> </td>
-        </tr>
-        <tr>
-          <td class="msg_info"> <img src="../assets/logo.png" width="50" height="50"><input type="hidden" value="2222">
-            <div class="user_info">
-              <h2>小李</h2>
-              <p>马哥你好，请问你多大了？</p>
-            </div>
+          <td class="msg_time">{{news.time}}</td>
+          <td class="msg_reply"><span v-show="news.reply">已回复</span></td>
+          <td class="msg_opr"> 
+            <button v-show="!news.collection" @click="CollectionTrue(index)" class="layui-btn layui-btn-mini layui-btn-normal msg_collect">
+              <i class="layui-icon"></i> 收藏
+            </button> 
+            <button v-show="news.collection" @click="CollectionFalse(index)" class="layui-btn layui-btn-mini layui-btn-normal msg_collect">
+              <i class="iconfont icon-star"></i> 已收藏
+            </button> 
+            <a class="layui-btn layui-btn-mini reply_msg"><i class="layui-icon"></i> 回复
+            </a> 
           </td>
-          <td class="msg_time">2017-05-27 12:50</td>
-          <td class="msg_reply"></td>
-          <td class="msg_opr"> <a class="layui-btn layui-btn-mini layui-btn-normal msg_collect"><i class="layui-icon"></i> 收藏</a> <a class="layui-btn layui-btn-mini reply_msg"><i class="layui-icon"></i> 回复</a> </td>
-        </tr>
-        <tr>
-          <td class="msg_info"> <img src="../assets/logo.png" width="50" height="50"><input type="hidden" value="3333">
-            <div class="user_info">
-              <h2>小赵</h2>
-              <p>马哥你好，请问你家是哪的？</p>
-            </div>
-          </td>
-          <td class="msg_time">2017-05-27 12:50</td>
-          <td class="msg_reply"></td>
-          <td class="msg_opr"> <a class="layui-btn layui-btn-mini layui-btn-normal msg_collect"><i class="layui-icon"></i> 收藏</a> <a class="layui-btn layui-btn-mini reply_msg"><i class="layui-icon"></i> 回复</a> </td>
-        </tr>
-        <tr>
-          <td class="msg_info"> <img src="../assets/logo.png" width="50" height="50"><input type="hidden" value="4444">
-            <div class="user_info">
-              <h2>小高</h2>
-              <p>马哥你好，请问你有没有女朋友啊？</p>
-            </div>
-          </td>
-          <td class="msg_time">2017-05-27 12:50</td>
-          <td class="msg_reply">已回复</td>
-          <td class="msg_opr"> <a class="layui-btn layui-btn-mini layui-btn-normal msg_collect"><i class="layui-icon"></i> 收藏</a> <a class="layui-btn layui-btn-mini reply_msg"><i class="layui-icon"></i> 回复</a> </td>
-        </tr>
-        <tr>
-          <td class="msg_info"> <img src="../assets/logo.png" width="50" height="50"><input type="hidden" value="5555">
-            <div class="user_info">
-              <h2>小楚</h2>
-              <p>马哥你好，请问你一个月工资多少啊？</p>
-            </div>
-          </td>
-          <td class="msg_time">2017-05-27 12:50</td>
-          <td class="msg_reply"></td>
-          <td class="msg_opr"> <a class="layui-btn layui-btn-mini layui-btn-normal msg_collect"><i class="layui-icon"></i> 收藏</a> <a class="layui-btn layui-btn-mini reply_msg"><i class="layui-icon"></i> 回复</a> </td>
         </tr>
       </tbody>
     </table>
@@ -69,60 +34,93 @@
 </template>
 
 <script>
-  export default {
+export default {
+  props: ["tablist","changellt"],
+  data() {
+    return {};
+  },
+  mounted() {
+   
+  },
+  methods:{
+    CollectionTrue(index) {
+      this.tablist[index].collection = true;
+    },
+    CollectionFalse(index) {
+      this.tablist[index].collection = false;
+    },
+    // collectData() {
+    //   for (let index = 0; index < this.tablist.length; index++) {
+    //     const element = this.tablist[index];
+    //     if (element.collection !== true) {
+    //       this.tablist.splice(index, 1);
+    //     }
+    //   }
+    // },
+    // changecoll(){
+    // console.log(this.changeCollect)
+    //   if(this.changeCollect===1){
+    //     console.log(1)
+    //   }
+    // }
+  },
+  computed:{
+     
+    
   }
+};
 </script>
 
 <style scoped>
 .selectMsg {
-    width: 110px;
-    margin-right: 15px;
-  }
-  .selectMsg select{
-    display: none;
-  }
-  .msg_info,
-  .msg_time,
-  .msg_reply,
-  .msg_opr {
-    min-height: 50px;
-    text-align: center;
-  }
-  .msg_info {
-    text-align: left;
-    position: relative;
-  }
-  .msg_info>img {
-    position: absolute;
-    left: 10px;
-    top: 10px;
-    cursor: pointer;
-  }
-  .msg_info .user_info {
-    padding-left: 55px;
-    line-height: 25px;
-  }
-  .msg_info .user_info h2 {
-    color: #222;
-    cursor: pointer;
-  }
-  .msg_info .user_info h2:hover {
-    color: #1AA094;
-  }
-  .msg_info .user_info p {
-    color: #8d8d8d;
-  }
-  .msg_reply {
-    color: #e15f63;
-  }
-  /*回复*/
-  .replay_edit {
-    overflow: hidden;
-    margin-bottom: 20px;
-  }
-  .replay_edit a {
-    margin-top: 10px;
-    width: 100px;
-    float: right;
-  }
+  width: 110px;
+  margin-right: 15px;
+}
+.selectMsg select {
+  display: none;
+}
+.msg_info,
+.msg_time,
+.msg_reply,
+.msg_opr {
+  min-height: 50px;
+  text-align: center;
+}
+.msg_info {
+  text-align: left;
+  position: relative;
+}
+.msg_info > img {
+  position: absolute;
+  left: 10px;
+  top: 10px;
+  cursor: pointer;
+}
+.msg_info .user_info {
+  padding-left: 55px;
+  line-height: 25px;
+}
+.msg_info .user_info h2 {
+  color: #222;
+  cursor: pointer;
+}
+.msg_info .user_info h2:hover {
+  color: #1aa094;
+}
+.msg_info .user_info p {
+  color: #8d8d8d;
+}
+.msg_reply {
+  color: #e15f63;
+}
+/*回复*/
+.replay_edit {
+  overflow: hidden;
+  margin-bottom: 20px;
+}
+.replay_edit a {
+  margin-top: 10px;
+  width: 100px;
+  float: right;
+}
 </style>
