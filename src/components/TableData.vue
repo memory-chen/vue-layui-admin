528db6b160fbeaf7262a53b0ca2eeeb979466c0a
beggarMen <1059326368@qq.com>
2017-11-16 21:02:40 +0800

    TableData.vue

--- src/components/TableData.vue
+++ src/components/TableData.vue
@@ -14,8 +14,8 @@
       <thead>
         <tr>
           <th>
-            <div @click="allChecked" v-show="AllcheckClick"  class="layui-unselect layui-form-checkbox layui-form-checked" lay-skin="primary"><i class="layui-icon"></i></div>
-            <div @click="allCheckedF" v-show="!AllcheckClick" class="layui-unselect layui-form-checkbox" lay-skin="primary"><i class="layui-icon"></i></div>
+            <div @click="allChecked" v-show="AllcheckClick" class="layui-unselect layui-form-checkbox layui-form-checked" lay-skin="primary"><i class="layui-icon"></i></div>
+            <div @click="allChecked" v-show="!AllcheckClick" class="layui-unselect layui-form-checkbox" lay-skin="primary"><i class="layui-icon"></i></div>
           </th>
           <th v-for="(table,index) in tableData.headTh" :key="table" style="text-align:left;">{{table}}</th>
         </tr>
@@ -23,7 +23,7 @@
       <tbody class="news_content">
         <tr v-for="(tabcont,index) in tableData.tableContent" :key="tabcont.title">
           <td>
-            <div @click="CheckClick(index)" v-show="tabcont.checked"  class="layui-unselect layui-form-checkbox layui-form-checked" lay-skin="primary"><i class="layui-icon"></i></div>
+            <div @click="CheckClick(index)" v-show="tabcont.checked" class="layui-unselect layui-form-checkbox layui-form-checked" lay-skin="primary"><i class="layui-icon"></i></div>
             <div @click="CheckClick(index)" v-show="!tabcont.checked" class="layui-unselect layui-form-checkbox" lay-skin="primary"><i class="layui-icon"></i></div>
           </td>
           <td align="left">{{tabcont.title}}</td>
@@ -43,94 +43,75 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      tableData: {
-        headTh: ["文章标题", "发布人", "审核状态", "浏览权限", "是否展示", "发布时间", "操作"],
-        // Allchecked: false,
-        tableContent: [
-          {
-            title: "css3用transition实现边框动画效果",
-            publisher: "请叫我马哥",
-            examine: "审核通过",
-            jurisdiction: "开放浏览",
-            exhibition: "1",
-            time: "2017-04-14",
-            checked: false
-          },
-          {
-            title: "css3用transition实现边框动画效果",
-            publisher: "请叫我马哥",
-            examine: "审核通过",
-            jurisdiction: "开放浏览",
-            exhibition: "1",
-            time: "2017-04-14",
-            checked: false
-          }
-        ]
-      }
-    };
-  },
-  methods: {
-    // 选中或取消选中
-    CheckClick(index) {
-      this.tableData.tableContent[index].checked = !this.tableData.tableContent[index].checked;
-      console.log(this.tableData.tableContent[index].checked)
-        // this.tableData.tableContent.forEach(element => {
-        //   if (element.checked === false) {
-        //     this.tableData.Allchecked = false;
-        //     return false;
-        //   }else {
-        //     alert(1)
-        //     this.tableData.Allchecked = true;
-        //   }
-        // });
+  export default {
+    data() {
+      return {
+        tableData: {
+          headTh: ["文章标题", "发布人", "审核状态", "浏览权限", "是否展示", "发布时间", "操作"],
+          // Allchecked: false,
+          tableContent: [{
+              title: "css3用transition实现边框动画效果",
+              publisher: "请叫我马哥",
+              examine: "审核通过",
+              jurisdiction: "开放浏览",
+              exhibition: "1",
+              time: "2017-04-14",
+              checked: false
+            },
+            {
+              title: "css3用transition实现边框动画效果",
+              publisher: "请叫我马哥",
+              examine: "审核通过",
+              jurisdiction: "开放浏览",
+              exhibition: "1",
+              time: "2017-04-14",
+              checked: false
+            }
+          ]
+        }
+      };
     },
-    // 全选、全不选
-    allChecked() {
-      // this.tableData.Allchecked = !this.tableData.Allchecked;
+    methods: {
+      // 选中或取消选中
+      CheckClick(index) {
+        this.tableData.tableContent[index].checked = !this.tableData.tableContent[index].checked;
+        // console.log(this.tableData.tableContent[index].checked)
+      },
+      // 全选、全不选
+      allChecked() {
         this.tableData.tableContent.forEach(element => {
           element.checked = true;
-          // alert(elementchecked)
         });
+      }
     },
-    allCheckedF() {
-      // this.tableData.Allchecked = !this.tableData.Allchecked;
-        this.tableData.tableContent.forEach(element => {
-          element.checked = false;
-          // alert(elementchecked)
-        });
-    },
-    
-  },
-  computed: {
+    computed: {
       AllcheckClick() {
-        this.tableData.tableContent.forEach(element => {
-          if (element.checked === false) {
-            return false;
-          }else {
-            return true;
+        var array = this.tableData.tableContent;
+        array.forEach(element => {
+          if (element.checked === true) {
+            return element.checked;
+          } else {
+            return element.checked;
           }
         });
       }
     },
-};
+  };
 </script>
 
 <style scoped>
-input[type="checkbox"] {
-  display: inline-block !important;
-  position: relative;
-  top: 0;
-  width: 16px;
-  height: 16px;
-  line-height: 16px;
-  border: 1px solid #d2d2d2;
-  font-size: 12px;
-  border-radius: 2px;
-  background-color: #fff !important;
-  -webkit-transition: 0.1s linear;
-  transition: 0.1s linear;
-}
+  input[type="checkbox"] {
+    display: inline-block !important;
+    position: relative;
+    top: 0;
+    width: 16px;
+    height: 16px;
+    line-height: 16px;
+    border: 1px solid #d2d2d2;
+    font-size: 12px;
+    border-radius: 2px;
+    background-color: #fff !important;
+    -webkit-transition: 0.1s linear;
+    transition: 0.1s linear;
+  }
 </style>
